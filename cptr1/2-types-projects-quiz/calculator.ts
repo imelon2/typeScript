5df88d1dfa3f1fe98c7d21230d7897b02299a99c
imelon2 <imelon279@naver.com>
2021-08-20 00:05:51 +0900

    8.20

--- cptr1/2-types-projects-quiz/calculator.ts
+++ cptr1/2-types-projects-quiz/calculator.ts
@@ -1,8 +1,31 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+{
+
+    type calcul = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder' ;
+
+    function calculate(state : calcul ,a : number, b : number) : number {
+        switch(state) {
+            case 'add':
+                return a + b;
+            case 'substract':
+                return a - b;
+            case 'multiply':
+                return a * b;
+            case 'divide':
+                return a / b;
+            case 'remainder':
+                return a % b;
+
+             default:
+                 throw Error("erroe!!");
+        }
+    }
+
+    console.log(calculate('add', 1, 3)); // 4
+    console.log(calculate('substract', 3, 1)); // 2
+    console.log(calculate('multiply', 4, 2)); // 8
+    console.log(calculate('divide', 4, 2)); // 2
+    console.log(calculate('remainder', 5, 2)); // 1
+}
