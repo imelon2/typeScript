02434a4ac80272232088e7851ff9b1ffe02a24c7
imelon2 <imelon279@naver.com>
2021-08-23 21:50:11 +0900

    8.23

--- cptr3/5-generic/5-1-function.ts
+++ cptr3/5-generic/5-1-function.ts
@@ -1,14 +1,14 @@
 {
   function checkNotNullBad(arg: number | null): number {
     if (arg == null) {
-      throw new Error('not valid number!');
+      throw new Error("not valid number!");
     }
     return arg;
   }
 
   function checkNotNullAnyBad(arg: any | null): any {
     if (arg == null) {
-      throw new Error('not valid number!');
+      throw new Error("not valid number!");
     }
     return arg;
   }
@@ -16,7 +16,7 @@
 
   function checkNotNull<T>(arg: T | null): T {
     if (arg == null) {
-      throw new Error('not valid number!');
+      throw new Error("not valid number!");
     }
     return arg;
   }
